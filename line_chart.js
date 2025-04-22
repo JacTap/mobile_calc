234c0d0c68052973f98909c2739176a8ba90be65
Jackie <jackie@tapcart.co>
2025-04-21 22:07:16 -0700

    test2

--- line_chart.js
+++ line_chart.js
@@ -1,21 +1,21 @@
-const labels = Utils.months({ count: 7 });
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
-      borderColor: "rgb(75, 192, 192)",
-      tension: 0.1,
-    },
-  ],
-};
+// const labels = Utils.months({ count: 7 });
+// const data = {
+//   labels: labels,
+//   datasets: [
+//     {
+//       label: "My First Dataset",
+//       data: [65, 59, 80, 81, 56, 55, 40],
+//       fill: false,
+//       borderColor: "rgb(75, 192, 192)",
+//       tension: 0.1,
+//     },
+//   ],
+// };
 
-const config = {
-  type: "line",
-  data: data,
-};
+// const config = {
+//   type: "line",
+//   data: data,
+// };
 
 const xValues = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15, 6];
