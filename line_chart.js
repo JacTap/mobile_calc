f44d6560ecdc8c7cb575c390b04098cceb061c63
Jackie <jackie@tapcart.co>
2025-04-21 23:38:50 -0700

    test7

--- line_chart.js
+++ line_chart.js
@@ -1,54 +1,74 @@
-// const labels = Utils.months({ count: 7 });
-// const data = {
-//   labels: labels,
-//   datasets: [
-//     {
-//       label: "My First Dataset",
-//       data: [65, 59, 80, 81, 56, 55, 40],
-//       fill: false,
-//       borderColor: "rgb(75, 192, 192)",
-//       tension: 0.1,
-//     },
-//   ],
-// };
+var ctx = document.getElementById("myChart").getContext("2d");
+/*** Gradient ***/
+var gradient = ctx.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, "rgb(50, 110, 250)");
+gradient.addColorStop(1, "rgba(103, 50, 250, 0)");
+/***************/
+const xValues = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15, 6];
 
-// const config = {
-//   type: "line",
-//   data: data,
-// };
+var data = {
+  labels: xValues,
+  datasets: [
+    {
+      fill: "start",
+      backgroundColor: gradient,
+      borderColor: "#6d53ff",
+      pointBackgroundColor: "#6d53ff",
+      pointBorderColor: "#6d53ff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "#ff6c23",
+      data: yValues,
+    },
+  ],
+};
 
-
-
-var line_chart = 'myChart'; // element id
-
-
-const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+var options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Custom Chart Title",
+    },
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+  //responsive: true,
+  //datasetStrokeWidth : 3,
+  //pointDotStrokeWidth : 4,
+  //tooltipFillColor: "rgba(0,0,0,0.8)",
+  //tooltipFontStyle: "bold",
+  //tooltipTemplate: "<%if (label){%><%=label + ' hod' %>: <%}%><%= value + '°C' %>",
+  //scaleLabel : "<%= Number(value).toFixed(0).replace('.', ',') + '°C'%>" ,
+  scales: {
+    y: {
+      min: 0,
+      max: 20,
+      ticks: {
+        stepSize: 2,
+      },
+    },
+  },
+};
 
 new Chart("myChart", {
   type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
-      data: yValues,
-      borderWidth: 8
-    }]
-  },
-
-  options: {
-    legend: {display: false},
-    scales: {
-      y: {
-        min: 0,
-        max: 20,
-        ticks: {
-          stepSize: 2
-        }
-      }
-    },
-  }
+  data: data,
+  options: options,
 });
